fdd75c0ce0c0f30c89d65915f2ac36e5dcadd425
Anshor <anshorjihadi@gmail.com>
2019-09-28 10:39:19 +0700

    Tugas 17 Js

---
+++ tugas17.js
@@ -0,0 +1,21 @@
+function tampil() {
+  var satu = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  var dua = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+  console.log(satu.toLowerCase());
+  console.log(satu.toUpperCase());
+  console.log(dua.toLowerCase());
+  console.log(dua.toUpperCase());
+}
+tampil()
+console.log("");
+var tiga = "Lorem Ipsum";
+
+var char0=tiga.charCodeAt(0);
+var char1=tiga.charCodeAt(1);
+var char2=tiga.charCodeAt(2);
+var char3=tiga.charCodeAt(3);
+var char4=tiga.charCodeAt(4);
+var char5=tiga.charCodeAt(5);
+var char6=tiga.charCodeAt(6);
+
+console.log(char0, char1, char2, char3, char4, char5, char6);
